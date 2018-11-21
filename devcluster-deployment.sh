13bea250c1abeaa09db585c3ffc85ccef433af34
Alex Miroshnyk <miroshnik.alex@gmail.com>
2018-11-21 17:24:34 +0100

    changed version. More parametarisation

--- devcluster-deployment.sh
+++ devcluster-deployment.sh
@@ -3,11 +3,13 @@
 PROJECT="k8s-gcp-project"
 CL_NAME="kubernetes-dev-cluster"
 REGION="europe-north1"
+NETWORK="gke-network"
+SUBNET="gke-network"
 ADM_NAME="admin"
 CL_VERSION="1.9.7-gke.11"
-VM_TYPE="g1-small"
+VM_TYPE="n1-standard-1"
 VM_IMG="COS"
-DISC_SIZE="50"
+DISC_SIZE="100"
 NUM_NODES="1"
 MIN_NODES="1"
 MAX_NODES="3"
@@ -15,7 +17,7 @@ NM_LIMITS="limits"
 NM_LIMITLESS="limitless"
 NM_MONITORING="monitoring"
 
-gcloud beta container --project "$PROJECT" clusters create "$CL_NAME" --region "$REGION" --username "$ADM_NAME" --cluster-version "$CL_VERSION" --machine-type "$VM_TYPE" --image-type "$VM_IMG" --disk-type "pd-standard" --disk-size "$DISC_SIZE" --scopes "https://www.googleapis.com/auth/devstorage.read_only","https://www.googleapis.com/auth/logging.write","https://www.googleapis.com/auth/monitoring","https://www.googleapis.com/auth/servicecontrol","https://www.googleapis.com/auth/service.management.readonly","https://www.googleapis.com/auth/trace.append" --preemptible --num-nodes "$NUM_NODES" --enable-cloud-logging --enable-cloud-monitoring --network "projects/k8s-gcp-project/global/networks/default" --subnetwork "projects/k8s-gcp-project/regions/europe-north1/subnetworks/default" --enable-autoscaling --min-nodes "$MIN_NODES" --max-nodes "$MAX_NODES" --addons HorizontalPodAutoscaling,HttpLoadBalancing --enable-autoupgrade --enable-autorepair --enable-ip-alias --metadata disable-legacy-endpoints=true --enable-network-policy 
+gcloud beta container --project "$PROJECT" clusters create "$CL_NAME" --region "$REGION" --username "$ADM_NAME" --cluster-version "$CL_VERSION" --machine-type "$VM_TYPE" --image-type "$VM_IMG" --disk-type "pd-standard" --disk-size "$DISC_SIZE" --scopes "https://www.googleapis.com/auth/devstorage.read_only","https://www.googleapis.com/auth/logging.write","https://www.googleapis.com/auth/monitoring","https://www.googleapis.com/auth/servicecontrol","https://www.googleapis.com/auth/service.management.readonly","https://www.googleapis.com/auth/trace.append" --preemptible --num-nodes "$NUM_NODES" --enable-cloud-logging --enable-cloud-monitoring --network "projects/$PROJECT/global/networks/$NETWORK" --subnetwork "projects/$PROJECT/regions/$REGION/subnetworks/$SUBNET" --enable-autoscaling --min-nodes "$MIN_NODES" --max-nodes "$MAX_NODES" --addons HorizontalPodAutoscaling,HttpLoadBalancing --enable-autoupgrade --enable-autorepair --enable-ip-alias --metadata disable-legacy-endpoints=true --enable-network-policy 
 
 echo "###### Configuring the Cluster ######"
 
